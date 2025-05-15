decdaf2526264b8772cc2aff2248a041ee59314d
LaxmiPrasanna-2593 <laxmiprasanna2593@gmail.com>
2025-05-15 18:25:44 +0530

    header footer addee

--- VVMAgro/urls.py
+++ VVMAgro/urls.py
@@ -8,6 +8,8 @@ from Inventory.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+        path('',home, name='home'),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
