798bcaff67d48c9060266f22ec6e62f220b14926
Durga Prakash Shilarapu <109608138+Durgaprakash1102@users.noreply.github.com>
2025-05-16 14:34:07 +0530

    settings

--- VVMAgro/settings.py
+++ VVMAgro/settings.py
@@ -75,23 +75,23 @@ WSGI_APPLICATION = 'VVMAgro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': '',
-#         'USER': '',
-#         'PASSWORD': '',
-#         'HOST': '',
-#         'PORT': '3306',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Agro',
+        'USER': 'root',
+        'PASSWORD': 'Durgaprakash@143',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
 
 
 
@@ -125,6 +125,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL = 'Inventory.CustomUser'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
