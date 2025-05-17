830425262743f7d51f593e2afe7c43051b33c1d6
LaxmiPrasanna-2593 <laxmiprasanna2593@gmail.com>
2025-05-17 18:34:04 +0530

    urls and views changed

--- VVMAgro/urls.py
+++ VVMAgro/urls.py
@@ -9,22 +9,20 @@ from Inventory.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home, name='home'),
-    path('aboutus',aboutus, name='aboutus'),
-    path('products',products, name='products'),
+    path('aboutus/',aboutus, name='aboutus'),
+    path('products/',products, name='products'),
     path('tractor/', tractor, name='tractor'),
     path('products/land-smoothing/', land_smoothing, name='land_smoothing'),
     path('pesticide/', pesticide_view, name='pesticide'),
     path('crop-harvester/', crop_harvester, name='crop_harvester'),
     path('rice-filtering/', rice_filtering, name='rice_filtering'),
     path('irrigation-system/', irrigation_system, name='irrigation_system'),
-    path('branchesanddistributors',branchesanddistributors, name='branchesanddistributors'),
-    path('careers',careers, name='careers'),
-    path('contactus',contactus, name='contactus'),
-<<<<<<< HEAD
-    path('tractorfarming',tractorproducts, name='tractorproducts'),
-    path('pesticidesprayer',pesticideprayer,name='pesticidesprayer'),
-    path('ricefiltering',ricefiltering,name='ricefiltering'),
-=======
+    path('branchesanddistributors/',branchesanddistributors, name='branchesanddistributors'),
+    path('careers/',careers, name='careers'),
+    path('contactus/',contactus, name='contactus'),
+    path('tractorfarming/',tractorproducts, name='tractorproducts'),
+    path('pesticidesprayer/',pesticideprayer,name='pesticidesprayer'),
+    path('ricefiltering/',ricefiltering,name='ricefiltering'),
     path('signup/', signup_view, name='signup'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
@@ -34,7 +32,6 @@ urlpatterns = [
     path('voucher/create/', voucher_create_view, name='voucher_create'),
     path('voucher/<int:voucher_id>/',voucher_detail_view, name='voucher_detail'),
 
->>>>>>> efd83b816cd5b1a9246892407f8538d9d6c624be
 
 
 ]
