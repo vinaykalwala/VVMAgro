57376b98d199a6a6ab38203108de9a53d5d0fd6e
Durga Prakash Shilarapu <109608138+Durgaprakash1102@users.noreply.github.com>
2025-06-10 10:56:41 +0530

    stock

--- Inventory/context_processors.py
+++ Inventory/context_processors.py
@@ -1,7 +1,8 @@
+from django.db.models import F
 from .models import Product
 
 def low_stock_count(request):
     if request.user.is_authenticated:
-        count = Product.objects.filter(stock_quantity__lt=10).count()
+        count = Product.objects.filter(stock_quantity__lt=F('stock_limit')).count()
         return {'low_stock_count': count}
     return {}
